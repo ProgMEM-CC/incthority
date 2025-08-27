3e47fd1ba1f44feab5ada6fa8b8a647dc594a348
paulnarag <naragpaulandrew@gmail.com>
2025-08-27 12:30:19 +0800

    Initial Commit

---
+++ incthority/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  incthority
+//
+//  Created by Paul Narag on 27/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ incthority/incthorityApp.swift
@@ -0,0 +1,17 @@
+//
+//  incthorityApp.swift
+//  incthority
+//
+//  Created by Paul Narag on 27/8/25.
+//
+
+import SwiftUI
+
+@main
+struct incthorityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
