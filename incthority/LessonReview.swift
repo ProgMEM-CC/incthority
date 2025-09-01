de705af76de921c2792f489dea82fdf7054c6962
ProgMEMCC <progmem.cc@gmail.com>
2025-09-01 17:27:42 +0800

    added pdfviewer sample.pdf is lesson 6 as a test

---
+++ incthority/LessonReview.swift
@@ -0,0 +1,18 @@
+//
+//  LessonReview.swift
+//  incthority
+//
+//  Created by Cory Chin on 1/9/25.
+//
+
+import SwiftUI
+
+struct LessonReview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LessonReview()
+}
